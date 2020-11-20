936212f2c52d8d41f8991c69d0b32fc6221de178
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2020-11-19 22:24:19 -0500

    Stub out basic controls
    Added code to handle most of the basic controls I've planned out.
    Currently they each call empty dummy functions.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 extern crate pancurses;
 
 use std::env;
+use std::fs;
 
 use pancurses::{endwin, initscr, noecho, Input};
 
+// TODO: Implement
+fn help_page() {}
+
+// TODO: Implement
+fn line_down() {}
+
+// TODO: Implement
+fn line_up() {}
+
+// TODO: Implement
+fn half_screen_down() {}
+
+// TODO: Implement
+fn half_screen_up() {}
+
+// TODO: Implement
+fn current_line_to_top() {}
+
+// TODO: Implement
+fn current_line_to_center() {}
+
 // Creates a secondary loop to collect and display search input
-// TODO: Convert from temporary function to actual search
+// Takes in a pointer to the current pancurses Window
+// TODO: Convert from temporary function to actual search 
 fn search(window: &pancurses::Window) {
     loop {
         match window.getch() {
@@ -26,9 +49,9 @@ fn search(window: &pancurses::Window) {
 // Main program loop
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let output = args[1].to_owned() + "\n";
+    let filename = args[1].to_owned();
     let window = initscr();
-    window.printw(&output);
+    window.printw(&filename);
     window.refresh();
     window.keypad(true);
     noecho();
@@ -37,6 +60,34 @@ fn main() {
             // q - Quit
             Some(Input::Character('q')) => {
                 break;
+            }
+            // h - Open help page
+            Some(Input::Character('h')) => {
+                help_page();
+            }
+            // j - Move down one line
+            Some(Input::Character('j')) => {
+                line_down();
+            }
+            // k - Move up one line
+            Some(Input::Character('k')) => {
+                line_up();
+            }
+            // d - Move down half screen
+            Some(Input::Character('d')) => {
+                half_screen_down();
+            }
+            // u - Move up half screen
+            Some(Input::Character('u')) => {
+                half_screen_up();
+            }
+            // z - Move the current line to the top of the screen
+            Some(Input::Character('z')) => {
+                current_line_to_top();
+            }
+            // Z - Center the current line
+            Some(Input::Character('Z')) => {
+                current_line_to_center();
             }
             // / - Will be search, for now just starts text input for testing
             Some(Input::Character('/')) => {
