59ac660e6b1e1e1d20c2a3a65aa81912bf93bed9
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2020-11-19 21:26:54 -0500

    Create basic testbed program skeleton
    This commit converts the sample pancurses program from the last commit
    into a basic skeleton for least. Now takes a command line argument (just
    prints it out for now). q quits the program. / temporarily enables text
    input as a placeholder for search input.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
 extern crate pancurses;
 
-use pancurses::{initscr, endwin, Input, noecho};
+use std::env;
 
+use pancurses::{endwin, initscr, noecho, Input};
+
+// Creates a secondary loop to collect and display search input
+// TODO: Convert from temporary function to actual search
+fn search(window: &pancurses::Window) {
+    loop {
+        match window.getch() {
+            Some(Input::Character('/')) => {
+                break;
+            }
+            Some(Input::Character(c)) => {
+                window.addch(c);
+            }
+            Some(input) => {
+                window.addstr(&format!("{:?}", input));
+            }
+            None => (),
+        }
+    }
+}
+
+// Main program loop
 fn main() {
-  let window = initscr();
-  window.printw("Type things, press delete to quit\n");
-  window.refresh();
-  window.keypad(true);
-  noecho();
-  loop {
-      match window.getch() {
-          Some(Input::Character(c)) => { window.addch(c); },
-          Some(Input::KeyDC) => break,
-          Some(input) => { window.addstr(&format!("{:?}", input)); },
-          None => ()
-      }
-  }
-  endwin();
+    let args: Vec<String> = env::args().collect();
+    let output = args[1].to_owned() + "\n";
+    let window = initscr();
+    window.printw(&output);
+    window.refresh();
+    window.keypad(true);
+    noecho();
+    loop {
+        match window.getch() {
+            // q - Quit
+            Some(Input::Character('q')) => {
+                break;
+            }
+            // / - Will be search, for now just starts text input for testing
+            Some(Input::Character('/')) => {
+                search(&window);
+            }
+            Some(Input::Character(_c)) => {
+                // window.addch(c);
+                window.addch('x');
+            }
+            Some(input) => {
+                window.addstr(&format!("{:?}", input));
+            }
+            None => (),
+        }
+    }
+    endwin();
 }
