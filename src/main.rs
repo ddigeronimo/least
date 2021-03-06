3b4842169a53f7efef91211be020a1bef3176483
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2021-03-06 02:19:55 -0500

    Update help message with new controls

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,10 @@ const HELP_MESSAGE: &str = r#"
                        - d, PgDn - Down half a screen
                        - u, PgUp - Up half a screen
                        - g - Jump to top of file
+                       - / - Forward search
+                       - ? - Backward search
+                       - n - Next search result
+                       - N - Last search result
                        - o - Open a new file
                        - h - Open help screen
 "#;
